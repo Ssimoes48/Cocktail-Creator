f857555728592a6eaac2c11458b274bf84ec9742
unknown <elliottmcf@gmail.com>
2021-01-27 12:52:50 -0500

    Adds second dropdown for ingredients and updates bar chart on cocktail selection

--- static/js/barChart.js
+++ static/js/barChart.js
@@ -1,67 +1,145 @@
-d3.csv("/RUT-SOM-DATA-PT-09-2020-U-C/Cocktail-Creator/Data/mr_boston_flattened_cleaned.csv").then(function (cocktailData) {
-    cocktailData.forEach(function(data) {
-        if (data.name == "Dry Martini") {
+// d3.csv("/RUT-SOM-DATA-PT-09-2020-U-C/Cocktail-Creator/Data/mr_boston_flattened_cleaned.csv").then(function (cocktailData) {
+//     // var ingredients = cocktailData.map(data => data["ingredient-2"]);
+//     ingredientColumns = ["ingredient-1", "ingredient-2", "ingredient-3", "ingredient-4", "ingredient-5", "ingredient-6"];
+//     measurementColumns = ["measurement-1", "measurement-2", "measurement-3", "measurement-4", "measurement-5", "measurement-6"];
+    
+//     ingredients = [];
+//     measurements = [];
 
-            var traces = [];
+//     cocktailData.forEach(function(data) {
 
-            ingredientColumns = ["ingredient-1", "ingredient-2", "ingredient-3", "ingredient-4", "ingredient-5", "ingredient-6"];
-            measurementColumns = ["measurement-1", "measurement-2", "measurement-3", "measurement-4", "measurement-5", "measurement-6"];
-            
-            for (i=0; i<6; i++) {
-                if (data[ingredientColumns[i]]) {
-                    var trace = {
-                        x: [data.name],
-                        y: [parseFloat(data[measurementColumns[i]])],
-                        name: `oz ${data[ingredientColumns[i]]}`,
-                        type: 'bar',
-                        width: 0.2,
-                    };
+
+//         for (i=0; i<6; i++) {
+//             if ((data[ingredientColumns[i]]) && !(ingredients.includes(data[ingredientColumns[i]]))) {
+//                 ingredients.push(data[ingredientColumns[i]]);
+//             }
+//         }
+    
+//         if (data.name == "Dry Martini") {
+
+//             var traces = [];
+
+
+//             for (i=0; i<6; i++) {
+//                 if (data[ingredientColumns[i]]) {
+//                     var trace = {
+//                         x: [data.name],
+//                         y: [parseFloat(data[measurementColumns[i]])],
+//                         name: `oz ${data[ingredientColumns[i]]}`,
+//                         type: 'bar',
+//                         width: 0.2,
+//                     };
                     
-                    traces.push(trace);
-                }
-            }
-            console.log(traces);
-            layout = {
-                barmode: 'stack',
-                // xaxis: {
-                //     visible: false,
-                // },
-                yaxis: {
-                    visible: false,
-                },
-                showlegend: false
-            }
-            Plotly.newPlot('bar', traces, layout);
-        }
-    });
-})
+//                     traces.push(trace);
+//                 }
+//             }
+
+//             layout = {
+//                 barmode: 'stack',
+//                 // xaxis: {
+//                 //     visible: false,
+//                 // },
+//                 yaxis: {
+//                     visible: false,
+//                 },
+//                 showlegend: false
+//             }
+//             Plotly.newPlot('bar', traces, layout);
+//         }
+//     });
+//     console.log(ingredients.sort());
+// })
 
 function init() {
     // Read json data
     d3.csv("/RUT-SOM-DATA-PT-09-2020-U-C/Cocktail-Creator/Data/mr_boston_flattened_cleaned.csv").then(function(cocktailData) {
-        
-        names = cocktailData.map(data => data.name);
-        console.log(names);
+        ingredientColumns = ["ingredient-1", "ingredient-2", "ingredient-3", "ingredient-4", "ingredient-5", "ingredient-6"];
+        measurementColumns = ["measurement-1", "measurement-2", "measurement-3", "measurement-4", "measurement-5", "measurement-6"];
 
+        ingredients = [];
+        cocktailData.forEach(function(data) {
+            for (i=0; i<6; i++) {
+                if ((data[ingredientColumns[i]]) && !(ingredients.includes(data[ingredientColumns[i]]))) {
+                    ingredients.push(data[ingredientColumns[i]]);
+                }
+            }
+        });
+
+        names = cocktailData.map(data => data.name);
 
         // Add dropdown option for each sample
-        var dropdownMenu = d3.select("#selCocktail");
+        var cocktailDropdown = d3.select("#selCocktail");
 
-        dropdownMenu.selectAll("option")
-            .data(names)
+        cocktailDropdown.selectAll("option")
+            .data(names.sort())
             .enter()
             .append("option")
             .attr("value", name => name)
             .text(name => name);
         
-        var currentSelection = dropdownMenu.node().value;
+        var currentCocktail = cocktailDropdown.node().value;
+
+        var ingredientDropdown = d3.select("#selIngredient");
+        ingredientDropdown.selectAll("option")
+            .data(ingredients.sort())
+            .enter()
+            .append("option")
+            .attr("value", ingredient => ingredient)
+            .text(ingredient => ingredient);
+
+        var currentIngredient = ingredientDropdown.node().value;
+
+        buildBarChart(currentCocktail);
 
     });
 }
 
-function optionChanged(newSample){
-    
+function cocktailChanged(newCocktail){
+    buildBarChart(newCocktail);
 }
 
+function ingredientChanged(newIngredient){
 
-init()
+}
+
+function buildBarChart(cocktail) {
+    d3.csv("/RUT-SOM-DATA-PT-09-2020-U-C/Cocktail-Creator/Data/mr_boston_flattened_cleaned.csv").then(function (cocktailData) {
+        // var ingredients = cocktailData.map(data => data["ingredient-2"]);
+        ingredientColumns = ["ingredient-1", "ingredient-2", "ingredient-3", "ingredient-4", "ingredient-5", "ingredient-6"];
+        measurementColumns = ["measurement-1", "measurement-2", "measurement-3", "measurement-4", "measurement-5", "measurement-6"];
+        var currentCocktail = cocktailData.filter(d => d.name === cocktail);
+        ingredients = [];
+        measurements = [];
+
+        var traces = [];
+    
+        for (i=0; i<6; i++) {
+            if (currentCocktail[0][ingredientColumns[i]]) {
+                var trace = {
+                    x: [currentCocktail[0].name],
+                    y: [parseFloat(currentCocktail[0][measurementColumns[i]])],
+                    name: `oz ${currentCocktail[0][ingredientColumns[i]]}`,
+                    type: 'bar',
+                    width: 0.2,
+                };
+                
+                traces.push(trace);
+            }
+        }
+
+        layout = {
+            barmode: 'stack',
+            title: currentCocktail[0].name,
+            xaxis: {
+                visible: false,
+            },
+            yaxis: {
+                visible: false,
+            },
+            showlegend: false
+        }
+        Plotly.newPlot('bar', traces, layout);
+    })
+}
+
+init();
