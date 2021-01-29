c9823970cfaaa912615de53ccddda721f73c4e31
Ssimoes48 <pbear48@aol.com>
2021-01-28 20:11:07 -0500

    trying to copy code

---
+++ webdesign/static/js/js-templating.js
@@ -0,0 +1,24 @@
+var myName = [];
+var myType = [];
+
+cocktail_data.forEach(cocktail => {
+    myName.push(cocktail.name);
+    myType.push(cocktail.type);
+});
+
+var data = [
+    {
+      x: myName,
+      y: myType,
+      marker:{
+        color: myName,
+      },
+      type: 'bar'
+    }
+  ];
+  
+  Plotly.newPlot('my-chart', data);
+
+  console.log(myData);
+  
+
