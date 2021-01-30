8d4550f0dd5ed1aaae5ade94cb8d01c53c5d4516
Tim Samson <9625746+timsamson@users.noreply.github.com>
2021-01-30 01:10:13 -0500

    cleaned up code

--- static/js/bubble.js
+++ static/js/bubble.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 d3.json("/bubble-data").then(function(bubbleData) {
   var cocktails = []
@@ -35,7 +34,7 @@ d3.json("/bubble-data").then(function(bubbleData) {
     },
     xaxis: {
       title: "Ingredients",
-      tickangle: "46"
+      tickangle: "45"
     
     },
     yaxis: {
@@ -45,41 +44,3 @@ d3.json("/bubble-data").then(function(bubbleData) {
 
 Plotly.newPlot("bubble", data, layout);
 });
-=======
-d3.json("/bubble-data", function(cocktailData) {
-    console.log(cocktailData);
-
-    var cocktails = [];
-    var ingredients = [];
-
-
-
-    cocktailData.filter(object => {
-      cocktails.push(object.cocktail);
-      ingredients.push(object.ingredient);
-    })
-
-
-    // create bubble chart
-    var data = [{
-      x: [ingredients],
-      y: [cocktails],
-      mode: "markers",
-      text: [ingredients],
-    }];
-
-    var layout = {
-      title: "All OTU's Sample Value",
-      height: 500,
-      width: 1050,
-      xaxis: {
-        title: "Ingredients",
-      },
-      yaxis: {
-        title: "Cocktails",
-      },
-    };
-    Plotly.newPlot("bubble", data, layout);
-
-})
->>>>>>> 762de0601c6661480f0498822e80e50f802d5af6
