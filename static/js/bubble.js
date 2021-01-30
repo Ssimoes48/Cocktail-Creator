8927914365e12e6e0caefe76b03cc4cb7e0244d5
Tim Samson <9625746+timsamson@users.noreply.github.com>
2021-01-30 00:50:54 -0500

    bubble.js with db query

--- static/js/bubble.js
+++ static/js/bubble.js
@@ -1,3 +1,51 @@
+<<<<<<< HEAD
+
+d3.json("/bubble-data").then(function(bubbleData) {
+  var cocktails = []
+  var ingredients = []
+  
+  bubbleData.forEach(function(data) {
+    cocktails.push(data.cocktail)
+    ingredients.push(data.ingredient)
+  });
+
+  console.log(cocktails)
+  console.log(ingredients)
+
+  var trace = {
+    x: ingredients,
+    y: cocktails,
+    mode: "markers",
+    marker: {
+      size: 15,
+      color: "#b300b3"
+    },
+    text: ingredients,
+  };
+
+  var data = [trace];
+
+  var layout = {
+    title: "Ingredient's in State's Popular Cocktails",
+    height: 600,
+    width: 1200,
+    margin: {
+      l:200,
+      b:150
+    },
+    xaxis: {
+      title: "Ingredients",
+      tickangle: "46"
+    
+    },
+    yaxis: {
+      title: "Cocktails",
+    },
+  };
+
+Plotly.newPlot("bubble", data, layout);
+});
+=======
 d3.json("/bubble-data", function(cocktailData) {
     console.log(cocktailData);
 
@@ -34,3 +82,4 @@ d3.json("/bubble-data", function(cocktailData) {
     Plotly.newPlot("bubble", data, layout);
 
 })
+>>>>>>> 762de0601c6661480f0498822e80e50f802d5af6
